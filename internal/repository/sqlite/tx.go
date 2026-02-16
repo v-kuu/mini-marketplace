d4bb24e064278650ffe4be9bc17feb6f6680a444
v-kuu <vilku@proton.me>
2026-02-16 18:11:53 +0200

    Fixed false transaction errors

--- internal/repository/sqlite/tx.go
+++ internal/repository/sqlite/tx.go
@@ -2,6 +2,7 @@ package sqlite
 import (
 	"context"
 	"database/sql"
+	"errors"
 )
 
 func withTx(ctx context.Context, db *sql.DB, fn func(tx *sql.Tx) error) error {
@@ -14,6 +15,11 @@ func withTx(ctx context.Context, db *sql.DB, fn func(tx *sql.Tx) error) error {
 	if err := fn(tx); err != nil {
 		return err
 	}
-
-	return tx.Commit()
+	if err := tx.Commit(); err != nil {
+		if errors.Is(err, sql.ErrTxDone) {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
